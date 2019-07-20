61ee5d99ccfb81704f69a4279c048c3413c07ef3
= <=>
2019-07-21 00:47:13 +0600

    room database updated

--- app/src/main/java/com/example/moon/roomdatabasedemo/dao/MyDao.java
+++ app/src/main/java/com/example/moon/roomdatabasedemo/dao/MyDao.java
@@ -12,15 +12,15 @@ import java.util.List;
 @Dao
 public interface MyDao {
     @Insert
-    public abstract void addUser(User user);
+     void addUser(User user);
 
     @Query("delete from user where uid = :myuid")
-    public abstract void removeUser(int myuid);
+     void removeUser(int myuid);
 
     @Query("select * from user")
-    public abstract List<User> getUsers();
+    List<User> getUsers();
 
     @Update
-    public abstract void updateUser(User user);
+     void updateUser(User user);
 
 }
